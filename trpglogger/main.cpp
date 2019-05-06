4f30e32e4167732421c93936d48df24928b819f4
w4123 <1840686745@qq.com>
2019-05-06 22:14:41 +0800

    减少代码冗余

--- trpglogger/main.cpp
+++ trpglogger/main.cpp
@@ -302,29 +302,7 @@ EVE_Exit(eventExit)
 	// 如果应用未启用, 则在eventDisable中已经释放过资源了，无需再次释放
 	if (Enabled)
 	{
-		// 释放Aws API资源
-		Aws::ShutdownAPI(options);
-
-		// 保存Session信息
-		std::ofstream saveSessionGroup(fileLoc + "Group.session", std::ios::out | std::ios::trunc);
-		if (saveSessionGroup)
-		{
-			for (const auto& ele : LogInfo)
-			{
-				saveSessionGroup << ele.first << " " << ele.second << std::endl;
-			}
-		}
-		saveSessionGroup.close();
-
-		std::ofstream saveSessionDiscuss(fileLoc + "Discuss.session", std::ios::out | std::ios::trunc);
-		if (saveSessionDiscuss)
-		{
-			for (const auto& ele : LogInfoDiscuss)
-			{
-				saveSessionDiscuss << ele.first << " " << ele.second << std::endl;
-			}
-		}
-		saveSessionDiscuss.close();
+		return eventDisable();
 	}
 	return 0;
 }
